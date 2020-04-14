eec1be740dda88b88deea92da79e8113c3107854
yichao.wu <yichao.wu@deepctrl.com>
2020-04-14 11:21:46 +0800

    init

---
+++ app/src/main/java/com/example/testscreenshot/ScreenShotJobIntentService.java
@@ -0,0 +1,12 @@
+package com.example.testscreenshot;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.JobIntentService;
+
+public class ScreenShotJobIntentService extends JobIntentService {
+    @Override
+    protected void onHandleWork(@NonNull Intent intent) {
+
+    }
+}
