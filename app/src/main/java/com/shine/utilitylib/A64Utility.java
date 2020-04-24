191651e9df15c8c00740e2b8203b9a2f637ea19c
yichao.wu <yichao.wu@deepctrl.com>
2020-04-24 17:34:54 +0800

    init

---
+++ app/src/main/java/com/shine/utilitylib/A64Utility.java
@@ -0,0 +1,17 @@
+package com.shine.utilitylib;
+
+/**
+ * 关机和开关屏命令
+ * A64 设备 需要在/system/lib64 有 相关库
+ */
+public class A64Utility {
+
+    static {
+        System.loadLibrary("A64Utility");
+    }
+
+    public native int OpenScreen();
+    public native int CloseScreen();
+    public native int Shutdown();
+
+}
